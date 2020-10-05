38644c66769fc90589efdfbf9adaf2617c26d7ba
Sugihara <s-koki.sugihara@rakuten.com>
2020-10-05 18:27:46 +0900

    task7

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>MyNews</title>
+        <title>Myprofile</title>
     </head>
     <body>
         <h1>Myprofle作成画面</h1>
