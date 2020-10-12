bd9493dc6b5b5ff656aee719e8e4a70eb92a98de
Sugihara <s-koki.sugihara@rakuten.com>
2020-10-12 17:32:20 +0900

    a

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
     Route::post('news/create', 'Admin\NewsController@create');
+    Route::get('profile/create', 'Admin\ProfileController@add');
     Route::post('profile/create', 'Admin\ProfileController@create'); 
     Route::post('profile/create', 'Admin\ProfileController@update');# 追記
 });
